76896a3929627b747ed5abb0fdf8ea3b5c225693
John Henry Niño Pico <jhonnino@unisangil.edu.co>
2022-11-08 13:02:40 -0500

    db connected

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 
+import productsRoutes from "./routes/products.routes";
+
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/',(req, res)=>{
-  res.json('on /')
-})
+app.get("/", (req, res) => {
+  res.json("on /");
+});
+
+app.use('/products',productsRoutes);
 
 export default app;
