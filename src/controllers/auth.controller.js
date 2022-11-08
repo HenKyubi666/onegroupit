30089143697a992325c9760c9ccec6512717aace
John Henry Niño Pico <jhonnino@unisangil.edu.co>
2022-11-08 18:42:39 -0500

    signin ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,8 +33,18 @@ export const signUp = async (req, res) => {
 
 export const signIn = async (req, res) => {
   const { email, password } = req.body;
-  const userFound = await User.findOne({ email });
+  const userFound = await User.findOne({ email }).populate("roles");
   if (!userFound) return res.status(400).json({ message: "User not found" });
-  console.log(userFound)
-  res.json({token: ''})
+
+  const matchPassword = await User.comparePassword(
+    req.body.password,
+    userFound.password
+  );
+
+  if (!matchPassword)
+    return res.status(401).json({ token: null, message: "Invalid password" });
+  const token = jwt.sign({ id: userFound._id }, config.SECRET, {
+    expiresIn: 86400,
+  });
+  res.json({ token });
 };
