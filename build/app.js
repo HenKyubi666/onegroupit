093ee8313d3e2858738f169bb01261105ccae0d0
John Henry Niño Pico <jhonnino@unisangil.edu.co>
2022-11-30 17:13:37 -0500

    clean code

--- build/app.js
+++ build/app.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 var _express = _interopRequireDefault(require("express"));
 var _morgan = _interopRequireDefault(require("morgan"));
+var _cors = _interopRequireDefault(require("cors"));
 var _products = _interopRequireDefault(require("./routes/products.routes"));
 var _auth = _interopRequireDefault(require("./routes/auth.routes"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
@@ -14,6 +15,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var app = (0, _express["default"])();
 app.use((0, _morgan["default"])("dev"));
 app.use(_express["default"].json());
+app.use((0, _cors["default"])());
 app.use("/api/products", _products["default"]);
 app.use("/api/auth", _auth["default"]);
 // app.use("/api/users", userRoutes);
--- build/controllers/products.controller.js
+++ build/controllers/products.controller.js
@@ -55,22 +55,37 @@ var createProduct = /*#__PURE__*/function () {
 exports.createProduct = createProduct;
 var getProducts = /*#__PURE__*/function () {
   var _ref2 = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee2(req, res) {
-    var products;
+    var userId, products;
     return _regeneratorRuntime().wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
-            _context2.next = 2;
-            return _Product["default"].find();
-          case 2:
-            products = _context2.sent;
-            res.json(products);
+            _context2.prev = 0;
+            userId = req.body.userId;
+            _context2.next = 4;
+            return _Product["default"].find({
+              userId: userId
+            });
           case 4:
+            products = _context2.sent;
+            res.json({
+              message: "Success",
+              productsData: products
+            });
+            _context2.next = 11;
+            break;
+          case 8:
+            _context2.prev = 8;
+            _context2.t0 = _context2["catch"](0);
+            res.json({
+              message: _context2.t0
+            });
+          case 11:
           case "end":
             return _context2.stop();
         }
       }
-    }, _callee2);
+    }, _callee2, null, [[0, 8]]);
   }));
   return function getProducts(_x3, _x4) {
     return _ref2.apply(this, arguments);
@@ -108,19 +123,31 @@ var updateProductById = /*#__PURE__*/function () {
       while (1) {
         switch (_context4.prev = _context4.next) {
           case 0:
-            _context4.next = 2;
+            _context4.prev = 0;
+            _context4.next = 3;
             return _Product["default"].findByIdAndUpdate(req.params.productId, req.body, {
               "new": true
             });
-          case 2:
+          case 3:
             updateProduct = _context4.sent;
-            res.json(updateProduct);
-          case 4:
+            res.json({
+              message: "Success",
+              productData: updateProduct
+            });
+            _context4.next = 10;
+            break;
+          case 7:
+            _context4.prev = 7;
+            _context4.t0 = _context4["catch"](0);
+            res.json({
+              message: _context4.t0
+            });
+          case 10:
           case "end":
             return _context4.stop();
         }
       }
-    }, _callee4);
+    }, _callee4, null, [[0, 7]]);
   }));
   return function updateProductById(_x7, _x8) {
     return _ref4.apply(this, arguments);
@@ -133,16 +160,27 @@ var deleteProductById = /*#__PURE__*/function () {
       while (1) {
         switch (_context5.prev = _context5.next) {
           case 0:
-            _context5.next = 2;
+            _context5.prev = 0;
+            _context5.next = 3;
             return _Product["default"].findByIdAndDelete(req.params.productId);
-          case 2:
-            res.status(204).json();
           case 3:
+            res.status(200).json({
+              message: "Success"
+            });
+            _context5.next = 9;
+            break;
+          case 6:
+            _context5.prev = 6;
+            _context5.t0 = _context5["catch"](0);
+            res.json({
+              message: _context5.t0
+            });
+          case 9:
           case "end":
             return _context5.stop();
         }
       }
-    }, _callee5);
+    }, _callee5, null, [[0, 6]]);
   }));
   return function deleteProductById(_x9, _x10) {
     return _ref5.apply(this, arguments);
