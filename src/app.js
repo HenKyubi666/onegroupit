fd9e51dcac422dfecd30a1cd3f9c5763deffd7c4
John Henry Niño Pico <jhonnino@unisangil.edu.co>
2022-12-01 00:53:24 -0500

    try fix cors 403 erro

--- src/app.js
+++ src/app.js
@@ -14,10 +14,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "https://master--onegroupit-frontend.netlify.app",
-      "http://localhost:3000",
-    ],
+    origin:
+      "https://master--onegroupit-frontend.netlify.app, http://localhost:3000",
     credentials: true,
   })
 );
